c2d12d11ca1c4b70fbc4e10d7a53a14ec643099c
chenjinquan <chenjinquan@banggood.com>
2018-09-26 17:24:16 +0800

    HttyMethod changed to enum

--- htty-core/src/main/java/com/github/cjqcn/htty/core/http/HttyMethod.java
+++ htty-core/src/main/java/com/github/cjqcn/htty/core/http/HttyMethod.java
@@ -6,28 +6,14 @@ package com.github.cjqcn.htty.core.http;
  * @author: chenjinquan
  * @create: 2018-09-14 16:42
  **/
-public class HttyMethod {
+public enum HttyMethod {
 
-
-	public static final HttyMethod GET = new HttyMethod("GET");
-	public static final HttyMethod POST = new HttyMethod("POST");
-	public static final HttyMethod HEAD = new HttyMethod("HEAD");
-
-	public static final HttyMethod OPTIONS = new HttyMethod("OPTIONS");
-	public static final HttyMethod DELETE = new HttyMethod("DELETE");
-	public static final HttyMethod PUT = new HttyMethod("PUT");
-
-
-	private String name;
-
-	private HttyMethod(String name) {
-		this.name = name;
-	}
+	GET, HEAD, POST, PUT, DELETE, OPTIONS;
 
 	public static final HttyMethod[] ALL_HTTP_METHOD = new HttyMethod[]{GET, POST, HEAD, OPTIONS, DELETE, PUT};
 
 	public static HttyMethod[] getAllHttpMethod() {
 		return ALL_HTTP_METHOD;
 	}
-
 }
+
