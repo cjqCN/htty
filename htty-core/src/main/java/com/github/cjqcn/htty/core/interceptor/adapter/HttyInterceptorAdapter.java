d052290a1d602a8193816e3319a260961163fa3f
chenjinquan <20170402cjq@gmail.com>
2018-10-13 10:19:42 +0800

    Update

--- htty-core/src/main/java/com/github/cjqcn/htty/core/interceptor/adapter/HttyInterceptorAdapter.java
+++ htty-core/src/main/java/com/github/cjqcn/htty/core/interceptor/adapter/HttyInterceptorAdapter.java
@@ -2,7 +2,30 @@ package com.github.cjqcn.htty.core.interceptor.adapter;
 
 import com.github.cjqcn.htty.core.common.Priority;
 import com.github.cjqcn.htty.core.common.UrlPatternMatchEnable;
+import com.github.cjqcn.htty.core.http.HttyRequest;
+import com.github.cjqcn.htty.core.http.HttyResponse;
 import com.github.cjqcn.htty.core.interceptor.HttyInterceptorWrapper;
 
+import java.util.Collection;
+
 public interface HttyInterceptorAdapter extends HttyInterceptorWrapper, Priority, UrlPatternMatchEnable {
+
+    @Override
+    default boolean preHandle(HttyRequest request, HttyResponse response) {
+        return true;
+    }
+
+    @Override
+    default void postHandle(HttyRequest request, HttyResponse response) {
+    }
+
+    @Override
+    int getPriority();
+
+    @Override
+    Collection<String> getIncludeUrlPatterns();
+
+    @Override
+    Collection<String> getExcludeUrlPatterns();
+
 }
